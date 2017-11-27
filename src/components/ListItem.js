9fc2a598aafebc8bab3a9cda221eb8d973688770
Ross Kiser <kiserr10@gmail.com>
2017-11-27 17:43:11 -0500

    tech stack app working with animations

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+	Text,
+	TouchableWithoutFeedback,
+	View,
+	LayoutAnimation
+} from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+	componentWillUpdate(){
+		LayoutAnimation.spring();
+	}
+	renderDescription(){
+		const { library, expanded } = this.props;
+
+		if(expanded){
+			return(
+				<CardSection style={{ flex: 1 }}>
+					<Text>{library.description}</Text>
+				</CardSection>
+			);
+		}
+	}
+
 	render(){
+		const { titleStyle } = styles;
+		const { title, id } = this.props.library;
+
 		return(
-			<CardSection>
-				<Text>{this.props.library.title}</Text>
-			</CardSection>
+			<TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+				<View>
+					<CardSection>
+						<Text style={titleStyle}>
+							{title}
+						</Text>
+					</CardSection>
+					{this.renderDescription()}
+				</View>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
 
-export default ListItem;
+const styles = {
+	titleStyle: {
+		fontSize: 18,
+		paddingLeft: 15
+	}
+};
+
+const mapStateToProps = (state, ownProps) => {
+	const expanded = state.selectedLibraryId === ownProps.library.id;
+	return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
